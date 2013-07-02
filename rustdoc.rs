4fd4cf28e251058bdab537b5c63f9737426bff85
Corey Richardson <corey@octayn.net>
2013-07-01 20:59:31 -0400

    Use a sysroot

--- rustdoc.rs
+++ rustdoc.rs
@@ -12,6 +12,7 @@ fn get_ast_and_resolve(crate: &Path) -> (@ast::crate, rustc::middle::resolve::Cr
     let parsesess = parse::new_parse_sess(None);
     let sessopts = @driver::session::options {
         binary: @"rustdoc",
+        maybe_sysroot: Some(@std::os::self_exe_path().get().pop()),
         ..copy *rustc::driver::session::basic_options()
     };
 
@@ -23,12 +24,12 @@ fn get_ast_and_resolve(crate: &Path) -> (@ast::crate, rustc::middle::resolve::Cr
     crate = syntax::ext::expand::expand_crate(parsesess, ~[], crate);
     crate = front::config::strip_unconfigured_items(crate);
     crate = front::std_inject::maybe_inject_libstd_ref(sess, crate);
+
     let ast_map = syntax::ast_map::map_crate(sess.diagnostic(), crate);
-    metadata::creader::read_crates(sess.diagnostic(), crate, sess.cstore,
-                                          sess.filesearch,
-                                          driver::session::sess_os_to_meta_os(sess.targ_cfg.os),
-                                          sess.opts.is_static,
-                                          parse::token::get_ident_interner());
+    let meta_os = driver::session::sess_os_to_meta_os(sess.targ_cfg.os);
+    let id_int = parse::token::get_ident_interner();
+    metadata::creader::read_crates(sess.diagnostic(), crate, sess.cstore, sess.filesearch, meta_os,
+                                   sess.opts.is_static, id_int);
     let lang_items = middle::lang_items::collect_language_items(crate, sess);
     let cmap = middle::resolve::resolve_crate(sess, lang_items, crate);
     (crate, cmap)
@@ -36,5 +37,7 @@ fn get_ast_and_resolve(crate: &Path) -> (@ast::crate, rustc::middle::resolve::Cr
 
 fn main() {
     let cratename = Path(os::args()[1]);
-    println(fmt!("%?", get_ast_and_resolve(&cratename)));
+    let (crate, cmap) = get_ast_and_resolve(&cratename);
+    println(fmt!("%?", crate));
+    println(fmt!("%?", cmap));
 }
