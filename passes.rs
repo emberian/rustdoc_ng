e1fe0e75427b1ae5eaa8cd858e3dc8356575dfd1
Corey Richardson <corey@octayn.net>
2013-08-07 10:58:41 -0400

    wip

--- passes.rs
+++ passes.rs
@@ -2,32 +2,38 @@ use clean;
 use clean::Item;
 use plugins;
 use visit;
+use visit::DocVisitor;
 
 pub fn noop(_crate: &mut clean::Crate) -> plugins::PluginResult {
     None
 }
 
-pub fn strip_hidden(_crate: &mut clean::Crate) -> plugins::PluginResult {
+pub fn strip_hidden(crate: &mut clean::Crate) -> plugins::PluginResult {
     struct Stripper;
     impl visit::DocVisitor for Stripper {
         pub fn visit_item_contents(&mut self, i: &mut Item) -> bool {
             for attr in i.attrs.iter() {
                 match attr {
-                    clean::List(~"doc", ref l) => {
+                    &clean::List(~"doc", ref l) => {
                         for innerattr in l.iter() {
                             match innerattr {
-                                clean::Word(ref s) => {
-                                    if ~"hidden" == s {
-                                        return true;
-                                    }
-                                }
+                                &clean::Word(ref s) if "hidden" == *s => {
+                                    info!("found one in strip_hidden; returning false");
+                                    return false;
+                                },
+                                _ => (),
                             }
                         }
-                    }
+                    },
+                    _ => ()
                 }
             }
+            true
         }
     }
+    let mut stripper = Stripper;
+    stripper.visit_crate(crate);
+    None
 }
 
 pub fn clean_comments(_crate: &mut clean::Crate) -> plugins::PluginResult {
--- visit.rs
+++ visit.rs
@@ -1,18 +1,71 @@
+use std;
 use clean;
 use clean::Item;
 use std::iterator::Extendable;
 
-trait DocVisitor {
+pub trait DocVisitor {
+    /// Visit every item in a vector, doing filtering like visit_item
+    /// describes
+    pub fn visit_item_vec(&mut self, field: &mut ~[Item]) {
+        use std::util::swap;
+        let mut foo = ~[]; swap(field, &mut foo);
+        field.extend(&mut foo.consume_iter().filter_map(|x| self.visit_item(x)));
+    }
     /// Visit an item, returning Some(item) to replace the item in the AST,
     /// and None to remove it. Note that just returning the Item you're given
     /// is equivalent to not changing it.
-    pub fn visit_item(&mut self, mut i: Item) -> Option<Item> {
-        let r = self.visit_item_contents(&mut i);
-        if r {
-            Some(i)
-        } else {
-            None
+    pub fn visit_item(&mut self, mut item: Item) -> Option<Item> {
+        use clean::*;
+
+        let r = self.visit_item_contents(&mut item);
+        if !r {
+            return None;
         }
+
+        match item.inner {
+            StructItem(ref mut i) => {
+                self.visit_item_vec(&mut i.fields);
+            },
+            ModuleItem(ref mut i) => {
+                self.visit_mod(i);
+            },
+            EnumItem(ref mut i) => {
+                self.visit_item_vec(&mut i.variants);
+            },
+            TraitItem(ref mut i) => {
+                fn vtrm<T: DocVisitor>(this: &mut T, trm: TraitMethod) -> Option<TraitMethod> {
+                    match trm {
+                        Required(it) => {
+                            match this.visit_item(it) {
+                                Some(x) => return Some(Required(x)),
+                                None => return None,
+                            }
+                        },
+                        Provided(it) => {
+                            match this.visit_item(it) {
+                                Some(x) => return Some(Provided(x)),
+                                None => return None,
+                            }
+                        },
+                    }
+                }
+                let mut foo = ~[]; std::util::swap(&mut i.methods, &mut foo);
+                i.methods.extend(&mut foo.consume_iter().filter_map(|x| vtrm(self, x)));
+            },
+            ImplItem(ref mut i) => {
+                self.visit_item_vec(&mut i.methods);
+            },
+            VariantItem(ref mut i) => {
+                match i.kind {
+                    StructVariant(ref mut j) => {
+                        self.visit_item_vec(&mut j.fields);
+                    },
+                    _ => (),
+                };
+            },
+            _ => (),
+        };
+        Some(item)
     }
     /// Visit an item's contents. This is the default "user-overridable"
     /// method. Return true to keep the item in the AST, and false to remove
@@ -29,8 +82,7 @@ trait DocVisitor {
                        &mut m.statics, &mut m.traits, &mut m.impls, &mut m.view_items];
 
         for field in fields.consume_iter() { 
-            let mut foo = ~[]; swap(field, &mut foo);
-            field.extend(&mut foo.consume_iter().filter_map(|x| self.visit_item(x)));
+            self.visit_item_vec(field);
         }
 
         let mut foo = ~[]; swap(&mut m.mods, &mut foo);
@@ -39,10 +91,13 @@ trait DocVisitor {
                 clean::ModuleItem(ref mut m) => m,
                 _ => fail!("non-module in ModuleItem")
             };
+            info!("Visiting submodule %? (%?)",x.name,  m_);
             self.visit_mod(m_);
             }
             x
             }));
     }
-    pub fn visit_crate(&mut self, &mut clean::Crate);
+    pub fn visit_crate(&mut self, c: &mut clean::Crate) {
+        self.visit_mod(match c.module.inner { clean::ModuleItem(ref mut m) => m, _ => fail!() });
+    }
 }
